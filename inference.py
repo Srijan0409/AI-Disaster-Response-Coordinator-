92e25640f493cd67bc23e814aa43670b398c3459
Ayush Pathak <124654967+AyushPathak4650@users.noreply.github.com>
2026-04-08 23:52:39 +0530

    Update inference.py added env_url directly

--- inference.py
+++ inference.py
@@ -14,7 +14,7 @@ load_dotenv()
 # ─── Config ─────────────────────────────────────────────────────
 
 IMAGE_NAME   = os.getenv("LOCAL_IMAGE_NAME")
-ENV_URL      = os.getenv("ENV_URL")
+ENV_URL      = os.getenv("ENV_URL", "https://ayush4650-disaster-env-v2.hf.space")
 API_KEY      = os.getenv("HF_TOKEN") or os.getenv("API_KEY")
 API_BASE_URL = os.getenv("API_BASE_URL", "https://router.huggingface.co/v1")
 MODEL_NAME   = os.getenv("MODEL_NAME", "Qwen/Qwen2.5-72B-Instruct")
